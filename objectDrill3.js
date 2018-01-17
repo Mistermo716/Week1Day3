ae1029a5864848a0bdb73fae6c118f8f2a8bc93c
Zhou <540376482yzb@gmail.com>
2018-01-17 11:30:05 -0800

    iterate over obj

--- objectDrill3.js
+++ objectDrill3.js
@@ -7,3 +7,16 @@ const loaf = {
 };
 console.log(`Flour: ${loaf.flour} Water: ${loaf.water}`);
 console.log("Hydration: " + loaf.hydration());
+
+console.log("================")
+
+const myobj = {
+    foo:'myfoo',
+    bar: 'mybar',
+    fum: 'myfum',
+    quux: 'myquux',
+    spam: 'myspam'
+}
+for (let key in myobj){
+    console.log(key + " : " + myobj[key])
+}
