5447fe96524cd9a7ffb7efd537ae97e022131afa
admin <Mistermo716@gmail.com>
2018-01-17 11:03:30 -0800

    Student ID Search

--- objectDrill2.js
+++ objectDrill2.js
@@ -1,9 +1,9 @@
 function makeStudentReport(data) {
-    const arrStudent = [];
-    data.forEach(function(student){
-        arrStudent.push(`${student.name} : ${student.grade}`);
-    });
-    return arrStudent;
+  const arrStudent = [];
+  data.forEach(function(student){
+    arrStudent.push(`${student.name} : ${student.grade}`);
+  });
+  return arrStudent;
     
 }
 const data = [{name:'Zhou', grade: 'A'}, {name: 'Johnny Robot', grade: 'C'}];
@@ -11,24 +11,39 @@ console.log(makeStudentReport(data));
 console.log("=================")
 
 const sampleStudents = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology'
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics'
-    }
-  ];
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  }
+];
 
 function enrollSummer( students ) {
-    students.forEach( student => {
-        student.status = "In summer class"
-    })
-    return students
+  students.forEach( student => {
+    student.status = "In summer class"
+  })
+  return students
 }
 console.log(sampleStudents)
 enrollSummer(sampleStudents)
 console.log(sampleStudents)
+
+function findById(items, idNum)
+{
+    let itemCar;
+    items.forEach(item => {
+        if (item.id === idNum)
+        {
+            itemCar = item;
+        }
+    });
+    return itemCar;
+}
+console.log('=====================');
+const studentData = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}];
+console.log(findById(studentData, 2));
