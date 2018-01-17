a5945b77abe1cc3663caeee70cd2b6ccd13a5bdb
admin <Mistermo716@gmail.com>
2018-01-17 09:10:49 -0800

    Person Maker Function

--- objectDrill1.js
+++ objectDrill1.js
@@ -1,25 +1,32 @@
-function createMyObject()
-{
+function createMyObject() {
     return {
-        foo:'bar',
+        foo: 'bar',
         answerToUniverse: 42,
         'olly olly': 'oxen free',
-        sayHello()
-        {
+        sayHello() {
             return 'hello';
         }
     };
 };
 const test = createMyObject();
 
-function objectUpdater(obj){
+function objectUpdater(obj) {
     obj.foo = "foo"
     obj.bar = "bar"
     obj.bizz = "bizz"
     obj.bang = "bang"
-    return  obj
+    return obj
 }
 
 objectUpdater(test)
 
 console.log(test)
+
+function personMaker() {
+    var person = {
+        firstName: 'Paul',
+        lastName: 'Jones',
+        fullName() { return this.firstName + " " + this.lastName;}
+    };
+    return person;
+}
