012895b793136670502576ce2cbc73f3234014b4
Zhou <540376482yzb@gmail.com>
2018-01-17 09:44:59 -0800

    summer class enrollment

--- objectDrill2.js
+++ objectDrill2.js
@@ -8,3 +8,27 @@ function makeStudentReport(data) {
 }
 const data = [{name:'Zhou', grade: 'A'}, {name: 'Johnny Robot', grade: 'C'}];
 console.log(makeStudentReport(data));
+console.log("=================")
+
+const sampleStudents = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology'
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics'
+    }
+  ];
+
+function enrollSummer( students ) {
+    students.forEach( student => {
+        student.status = "In summer class"
+    })
+    return students
+}
+console.log(sampleStudents)
+enrollSummer(sampleStudents)
+console.log(sampleStudents)
