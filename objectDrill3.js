13d1ecec223943cfccf7ed9140784508baca29b5
Zhou <540376482yzb@gmail.com>
2018-01-17 12:48:36 -0800

    cypher

--- objectDrill3.js
+++ objectDrill3.js
@@ -33,12 +33,12 @@ const people = [
     {name:"Mo", jobTitle:"student",boss:"Boss Man"},
     {name:"Ray", jobTitle:"the last jedi",boss:"Boss Man"},
     {name:"Kobe Bryant", jobTitle:"pro basketball player",boss:"Boss Man"},
-    {name:"Boss Man", jobTitle:"CEO", boss:null}
+    {name:"Boss Man", jobTitle:"CEO"}
     
 
 ]
 people.forEach( person =>{
-    if(person.boss === null)
+    if(!person.hasOwnProperty('boss'))
     {
         console.log(`${person.jobTitle} ${person.name} doesn't report to anybody."`);
     }
@@ -47,5 +47,44 @@ people.forEach( person =>{
     console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`)
     }
 })
+
+console.log("===============================")
+
+function cypher( stringWord ) {
+    const codeBase = {
+        a: 2,
+        b: 3,
+        c: 4,
+        d: 5
+    }
+    let result = ""
+    return stringWord
+        .split(' ')
+        .reduce( (acc, curr) => {
+            let received = compareKey(codeBase, curr)
+            return acc + received
+        },"")
+}
+
+function compareKey(codeBase, word){
+    let result = ' ';
+    for(let key in codeBase){
+        if(key === word[0]){ 
+            const index = codeBase[key]
+            return result = word[index-1]
+        }
+        else{
+            result = ' '
+        }
+    }
+    // console.log(result)
+    return result
+}
+
+const message = "aS bsO dqweS fskfdkjf"
+
+console.log(cypher(message))
+
+
 
 
